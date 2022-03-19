53c6b3192bf7cdea5c36126d3c2cbcfdd83492e6
emamaria <emascript@gmail.com>
2022-03-19 20:56:48 +0100

    chore: add last commit

--- iteracion1.js
+++ iteracion1.js
@@ -19,10 +19,9 @@ window.onload = function() {
  })
 
 
- //1.3  Pone en el ejercicio que añada un evento input no sé si se refiere a lo mejor a change dejo puesto en chage
- // hasta resolver la duda
 
- document.querySelector(".value").addEventListener("change", function(event){
+
+ document.querySelector(".value").addEventListener("input", function(event){
     console.log(event.target.value);
    
 })
